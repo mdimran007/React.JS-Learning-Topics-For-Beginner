104270e2bd08714bfe4a69bdbd019a47ce650385
imran <imran@gmail.com>
2020-11-27 13:47:37 +0600

    Topic: Class Component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import './myfunctionalcomponents/FreshFunctionalComponent'
 import FreshComponent from './myfunctionalcomponents/FreshFunctionalComponent';
+import FreshClassComponent from './myclasscomponents/FreshClassComponent';
 
 function App() {
   return (
     <div className="App">
      <FreshComponent></FreshComponent>
+     <FreshClassComponent></FreshClassComponent>
     </div>
   );
 }
---
+++ src/myclasscomponents/FreshClassComponent.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react'
+
+class FreshClassComponent extends Component{
+
+    render() {
+        return (
+            <h1>Welcom Class Component.</h1>
+        )
+    }
+        
+    
+
+}
+
+export default FreshClassComponent
